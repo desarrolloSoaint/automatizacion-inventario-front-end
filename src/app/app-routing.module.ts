8359256a98e8bbc8e5f44a984de68bde90789a08
ErikaMata <emata@soaint.com>
2021-12-03 11:37:35 -0400

    componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ButtonComponent } from './components/button/button.component';
+import { TableListComponent } from './components/table-list/table-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'home', component:ButtonComponent, pathMatch: 'full'},
+  {path:'tablelist', component: TableListComponent},
+  {path:'**', pathMatch: 'full', redirectTo:'home'},
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AddComponent } from './components/add/add.component';
+import { TableListComponent } from './components/table-list/table-list.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ViewComponent } from './components/view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    NavbarComponent,
+    FooterComponent,
+    AddComponent,
+    TableListComponent,
+    EditComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
